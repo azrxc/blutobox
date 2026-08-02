a0b04a0f30d6fc9f961b938ddf08fa8c03a228a3
A2 <azrele2@gmail.com>
2026-08-02 12:24:32 +0800

    Fix loading icon badge to fixed black (was flipping to white in dark mode via theme-reactive accent color)

--- src/app/loading-icon.tsx
+++ src/app/loading-icon.tsx
@@ -1,7 +1,7 @@
 export function LoadingIcon({ size = 56 }: { size?: number }) {
   return (
     <span
-      className="inline-flex shrink-0 items-center justify-center rounded-full bg-accent p-2"
+      className="inline-flex shrink-0 items-center justify-center rounded-full bg-black p-2"
       style={{ width: size, height: size }}
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
